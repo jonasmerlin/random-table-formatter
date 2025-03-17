c40c8e95e53035e2527b472b5c466de21a0481d2
Jonas-Merlin Schumacher <jonas.merlin.schumacher@mailbox.org>
2025-03-17 21:00:59 +0100

    Add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import TableFormatter from "./App.tsx";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TableFormatter />
     <Toaster />
+    <Analytics />
   </StrictMode>,
 );
